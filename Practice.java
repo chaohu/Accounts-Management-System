a93ff8494756a3406b6beea0bfd07e568210e7e3
huchao <chaohu>
2016-03-17 23:15:22 +0800

    initial

---
+++ Practice.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Image;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+public  class Practice {
+	public static void main(String[] args) {
+		FisWindow x;
+		x = new FisWindow();
+		x.Fissss();
+	}
+}
+class FisWindow {
+	static JFrame jfrm = new JFrame("账单管理系统");
+	public void Fissss() {
+		Image im = (new ImageIcon("image/icon.png")).getImage();
+		jfrm.setIconImage(im);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
+		gd.setFullScreenWindow(jfrm);
+		jfrm.add(setjpan1());
+		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jfrm.setVisible(true);
+	}
+	JPanel setjpan1() {
+		JPanel jpan1 = new JPanel();
+		jpan1.setSize(100, 100);
+		jpan1.setLocation(0, 0);
+		JLabel lb = new JLabel();
+		Icon pic;
+		pic = new ImageIcon("t1.jpg");
+		lb.setIcon(pic);
+		jpan1.add(lb);
+		return jpan1;
+	}
+	JButton setbun() {
+		JButton bun = new JButton("确认");
+		bun.setSize(80, 20);
+		bun.setLocation(20, 200);
+		return bun;
+	}
+}
---
+++ image.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+public  class image extends JFrame {
+	JTabbedPane jtab = new JTabbedPane(JTabbedPane.TOP);
+	public image() {
+		JLabel[] lab = new JLabel[6];
+		Icon pic;
+		String title;
+		for (int i = 1; i <= 5; i++) {
+			pic = new ImageIcon("t" + i + ".jpg");
+			lab[i] = new JLabel();
+			lab[i].setIcon(pic);
+			title = "第" + i + "页";
+			jtab.add(title, lab[i]);
+		}
+		this.add(jtab);
+	}
+	public static  void main(String[] args) {
+		image frm = new image();
+		frm.setTitle("选项卡的应用");
+		frm.setSize(300, 300);
+		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frm.setVisible(true);
+	}
+}
