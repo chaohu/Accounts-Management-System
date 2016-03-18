f9ad0bd0473d57862deed21fe7a3d8df4c4abef8
huchao <chaohu>
2016-03-18 21:17:13 +0800

    Consummate the first page.

--- Practice.java
+++ Practice.java
@@ -12,40 +12,57 @@ public  class Practice {
 }
 class FisWindow {
 	static JFrame jfrm = new JFrame("财务管理系统");
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	public void Fissss() {
-		FlowLayout flow = new FlowLayout(FlowLayout.CENTER, 5, 10);
 		ImageIcon bgpic = new ImageIcon("image/background.jpg");
 		JLabel bglab = new JLabel(bgpic);
 		jfrm.getLayeredPane().add(bglab, new Integer(Integer.MIN_VALUE));
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		bgpic.setImage(bgpic.getImage().getScaledInstance(screenSize.width, screenSize.height, Image.SCALE_DEFAULT));
 		bglab.setBounds(0, 0, screenSize.width, screenSize.height);
 		Container cp = jfrm.getContentPane();
-		cp.setLayout(new BorderLayout());
+		cp.setLayout(null);
 		((JPanel)cp).setOpaque(false);
 		Image im = (new ImageIcon("image/icon.png")).getImage();
-		cp.setLayout(flow);
 		jfrm.setIconImage(im);
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		GraphicsDevice gd = ge.getDefaultScreenDevice();
 		gd.setFullScreenWindow(jfrm);
 		cp.add(setjpan1());
+		cp.add(setbunO());
+		cp.add(setbunI());
 		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jfrm.setVisible(true);
 	}
 	JPanel setjpan1() {
 		JPanel jpan1 = new JPanel();
-		jpan1.setSize(100, 100);
-		jpan1.setLocation(0, 500);
-		JLabel lb = new JLabel("财务管理系统");
-		lb.setFont(new Font("Dialog", 1, 30));
+		jpan1.setSize(screenSize.width, 150);
+		jpan1.setLocation(0, 80);
+		jpan1.setOpaque(false);
+		JLabel lb = new JLabel("财 务 管 理 系 统   ", JLabel.CENTER);
+		lb.setLayout(null);
+		lb.setLocation(0, 0);
+		lb.setFont(new Font("Dialog", 1, 100));
 		jpan1.add(lb);
 		return jpan1;
 	}
-	JButton setbun() {
-		JButton bun = new JButton("确认");
-		bun.setSize(80, 20);
-		bun.setLocation(100, 600);
-		return bun;
+	JButton setbunO() {
+		ImageIcon proout = new ImageIcon("image/proout.png");
+		JButton buno = new JButton( "产品出库", proout);
+		buno.setSize(300, proout.getIconHeight() + 10);
+		buno.setFont(new Font("Dialog", 1, 30));
+		buno.setContentAreaFilled(false);
+		buno.setBorderPainted(false);
+		buno.setLocation(520, 400);
+		return buno;
+	}
+	JButton setbunI() {
+		ImageIcon soradd = new ImageIcon("image/soradd.png");
+		JButton buni = new JButton( "原料入库", soradd);
+		buni.setSize(300, soradd.getIconHeight() + 10);
+		buni.setFont(new Font("Dialog", 1, 30));
+		buni.setContentAreaFilled(false);
+		buni.setBorderPainted(false);
+		buni.setLocation(100, 400);
+		return buni;
 	}
 }
